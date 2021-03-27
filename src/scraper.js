97f337cf3be87f7cf1257d9829ff56b427ff3f64
Kirby <kirbyc6@gmail.com>
2021-03-27 15:59:50 -0400

    scraper

--- src/scraper.js
+++ src/scraper.js
@@ -1,19 +1,25 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 async function startScraper() {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    
-    await page.goto('https://am-i-eligible.covid19vaccine.health.ny.gov/');
-    
-console.log('before run')
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-const [data] = await page.$x('//*[@id="statePods_table"]/tbody/tr[1]/td[1]');
-const src = await data.getProperty('src');
-const srcTxt = await src.jsonValue();
-console.log(srcTxt);
+  await page.goto("https://am-i-eligible.covid19vaccine.health.ny.gov/");
 
-await browser.close();
+  console.log("before run");
+
+  const allNodes = await page.evaluate(() => {
+    const elements = [
+      ...document.querySelectorAll(
+        "#cntContent_lstMain tr:not(:first-child) td:nth-child(2)"
+      ),
+    ];
+    return elements.map((element) => element.textContent);
+  });
+
+  console.log(allNodes);
+
+  await browser.close();
 }
 
 startScraper();
