18140951aa4c8144f0cab631d0b9056b06962ee5
Kirby <kirbyc6@gmail.com>
2021-03-27 16:20:52 -0400

    sracper

--- src/scraper.js
+++ src/scraper.js
@@ -4,15 +4,18 @@ async function startScraper() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto("https://am-i-eligible.covid19vaccine.health.ny.gov/");
+  await page.goto("https://am-i-eligible.covid19vaccine.health.ny.gov/", {
+    waitUntil: "networkidle2",
+  });
 
   console.log("before run");
 
   const allNodes = await page.evaluate(() => {
-    const elements = [
-      ...document.querySelectorAll(
-        "#cntContent_lstMain tr:not(:first-child) td:nth-child(2)"
-      ),
+    let holderArray = [];
+    let result = [];
+    let counter = 1;
+    let elements = [
+      ...document.querySelectorAll("#statePods_table tbody tr td"),
     ];
     return elements.map((element) => element.textContent);
   });
